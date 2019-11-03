f281a898518699395eb12fbc8d2e487638b9d2cc
Colin Hunt <colin.hunt@live.com>
2019-11-03 15:40:21 -0700

    Refactoring BowlingMatch.

--- src/main/java/org/colinhunt/bowlingscorer/bowlingmatch/BowlingMatch.java
+++ src/main/java/org/colinhunt/bowlingscorer/bowlingmatch/BowlingMatch.java
@@ -1,18 +1,20 @@
 package org.colinhunt.bowlingscorer.bowlingmatch;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.colinhunt.bowlingscorer.player.Player;
 
 public class BowlingMatch
 {
-    private Player lastPlayer;
-    private Player currentPlayer;
+    private final List<Player> players;
     private int rolls = 0;
-
+    private int currentPlayerIndex = 0;
 
     public BowlingMatch(Player player1, Player player2)
     {
-        this.currentPlayer = player1;
-        this.lastPlayer = player2;
+        this.players = new ArrayList<>(Arrays.asList(player1, player2));
     }
 
     public boolean isOver()
@@ -22,16 +24,14 @@ public class BowlingMatch
 
     public Player currentPlayer()
     {
-        return currentPlayer;
+        return players.get(currentPlayerIndex);
     }
 
     public void roll(int pinsDown)
     {
         if (++rolls % 2 == 0)
         {
-            Player nextPlayer = lastPlayer;
-            lastPlayer = currentPlayer;
-            currentPlayer = nextPlayer;
+            currentPlayerIndex = (currentPlayerIndex + 1) % 2;
         }
     }
 
